e0fe87056b709f3b2868e8fbecb0c15e4c4bdce6
naselin <nikita.selin@gmail.com>
2022-12-11 13:59:39 +0300

    HW-13 (lesson-18). First commit.

---
+++ src/post-py/helpers.py
@@ -0,0 +1,39 @@
+import structlog
+from pymongo import MongoClient
+from pymongo.errors import ConnectionFailure
+from json import dumps
+from flask import request
+
+
+log = structlog.get_logger()
+
+
+def http_healthcheck_handler(mongo_host, mongo_port, version):
+    postdb = MongoClient(mongo_host, int(mongo_port),
+                         serverSelectionTimeoutMS=2000)
+    try:
+        postdb.admin.command('ismaster')
+        postdb_status = 1
+    except ConnectionFailure:
+        postdb_status = 0
+
+    status = postdb_status
+    healthcheck = {
+        'status': status,
+        'dependent_services': {
+            'postdb': postdb_status
+        },
+        'version': version
+    }
+    return dumps(healthcheck)
+
+
+def log_event(event_type, name, message, params={}):
+    request_id = request.headers['Request-Id'] \
+        if 'Request-Id' in request.headers else None
+    if event_type == 'info':
+        log.info(name, service='post', request_id=request_id,
+                 message=message, params=params)
+    elif event_type == 'error':
+        log.error(name, service='post', request_id=request_id,
+                  message=message, params=params)
